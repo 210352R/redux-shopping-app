13df08e891978eced3c0b6e42a1031c3987ebb3c
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-09-08 21:49:48 +0530

    update combined cart reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { productsReducer, selectedProductReducer } from "./productReducer";
+import { cartReducer } from "./cartReducer";
 
 const reducers = combineReducers({
   allProducts: productsReducer,
   product: selectedProductReducer,
+  cart: cartReducer,
 });
 export default reducers;
